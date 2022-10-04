bbd98a4a1cb7f778689b1aff72249afc48ebee21
noremacsim <mrcameronsim@gmail.com>
2022-10-04 20:31:09 +0100

    update

--- App/controllers/xtreamController.js
+++ App/controllers/xtreamController.js
@@ -119,7 +119,7 @@ module.exports = {
         updatedFile.write('#EXT-X-VERSION:3 \n');
         updatedFile.write(`${lines[2]} \n`);
         updatedFile.write('#EXT-X-ALLOW-CACHE:NO \n');
-        updatedFile.write('#EXT-X-TARGETDURATION:8 \n');
+        updatedFile.write('#EXT-X-TARGETDURATION:10 \n');
         updatedFile.write('#EXTINF:10.000000, \n');
         updatedFile.write(result.items[0].url);
         updatedFile.end();
@@ -178,9 +178,8 @@ module.exports = {
     // multiple devices
     tempFileName = `${username}${Date.now()}.m3u8`
     await processm3u8(tempFileName);
-    await getUpdated(tempFileName);
 
-    let streamT = await fs.createReadStream(path.join(__dirname, `../Temp/1${tempFileName}`));
+    let streamT = await fs.createReadStream(path.join(__dirname, `../Temp/${tempFileName}`));
     let streamDataT = await new Readable().wrap(streamT);
     console.log('Updating');
     return h.response(streamDataT)
