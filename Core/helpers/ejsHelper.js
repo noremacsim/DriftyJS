3db717ea6a74dcaad15ce79ede710260072c4f3f
noremacsim <mrcameronsim@gmail.com>
2022-11-29 23:16:25 +0000

    Remove Uneccasery Files

--- Core/helpers/ejsHelper.js
+++
@@ -1,9 +0,0 @@
-module.exports.name = 'ejsHelpers';
-module.exports.functions = {
-    isLoggedIn: function (request) {
-        return request.state.isLoggedIn;
-    },
-    twoFAPassed: function (request) {
-        return request.state.twoFAPassed;
-    },
-};
