6767f03495f00aee87df0aa2f19613c636e54242
noremacsim <mrcameronsim@gmail.com>
2022-11-29 12:58:48 +0000

    Add In error Route and 404 Page

---
+++ App/routes/error.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const Boom = require("boom");
+
+module.exports = [
+    {
+        method: [ 'GET', 'POST' ],
+        path: '/{any*}',
+        handler: (request, h) => {
+            const accept = request.headers.accept
+
+            if (accept && accept.match(/json/)) {
+                return Boom.notFound('This resource isn’t available.')
+            }
+
+            return h.fullView('error/404', null).code(404);
+
+        }
+    },
+];
